3555ee9733149aa841cfd90bd89cb45b6468fedf
22tooty2 <yonatanab1@gmail.com>
2022-11-14 03:32:12 -0800

    Finished Rock Paper Scissors

--- javascript.js
+++ javascript.js
@@ -10,17 +10,17 @@ let ComputerChoice = 0;
 
 let Winner = "";
 
+let Result = 0; //0 - TIE, 1 - USER WON, 2 - COMPUTER WON
+
 function playRound() {
     console.log("New Round!");
+    window.alert("New Round!");
     Rounds++;
 
-    if (UserScore >= 3 || ComputerScore >= 3)
-    {
-        Out();
-    }
+    window.alert("Scores: " + UserScore + " " + ComputerScore);
 
     Choice = prompt("What is your move?");
-    FullChoice.toUpperCase();
+    FullChoice = Choice.toUpperCase();
 
     ComputerChoice = Math.floor(Math.random() * 3);
 
@@ -34,8 +34,55 @@ function playRound() {
         case "SCISSORS":
             UserChoice = 2;
             break;
+        default:
+            window.alert("NOT DETECTED, COMPUTER WINS THIS ROUND.");
     }
 
+    alert("You Chose " + Choice);
+    
+    var computerChoiceString = "";
+    if (ComputerChoice == 0)
+        computerChoiceString = "Rock";
+    else if (ComputerChoice == 1)
+        computerChoiceString = "Paper";
+    else
+        computerChoiceString = "Scissors";
+
+
+    alert("The Computer Chose " + computerChoiceString);
+
+    if (UserChoice == ComputerChoice)
+    {
+        alert("TIE!");
+        return;
+    }
+    if (UserChoice > ComputerChoice && ComputerChoice != 0)
+    {
+        UserScore++;
+        alert("You Won The Round!");
+    }
+    else if (ComputerChoice > UserChoice && UserChoice != 0)
+    {
+        ComputerScore++;
+        alert("The Computer Won The Round!");
+    }
+    else if (UserChoice == 0)
+    {
+        UserScore++;
+        alert("You Won The Round!");
+    }
+    else
+    {
+        ComputerScore++;
+        alert("The Computer Won The Round!");
+    }
+}
+
+function CheckWinner() {
+    if (UserScore >= 3 || ComputerScore >= 3)
+    {
+        Out();
+    }
 }
 
 function Out() {
@@ -44,21 +91,33 @@ function Out() {
     {
         Winner = "Tie";
     }
-    window.alert("The ", Winner, " Won! ", UserScore, " : ", ComputerScore);
+    window.alert("The " + Winner + " Won! " + UserScore + " : " + ComputerScore);
 }
 
 function Main() {
+    window.alert("Welcome to Rock Paper Scissors!");
+
     while (true)
     {
-        
+        while (Rounds < 5)
+        {
+            playRound();
+            Rounds++;
+            CheckWinner();
+        }
 
-        if (prompt("Continue?") != toUpperCase("Yes"))
+        if (prompt("Continue?").toUpperCase() != "YES")
         {
             window.alert("Thank you for Playing!");
             return;
         }
+
+        Rounds = 0;
+        UserScore = 0;
+        ComputerScore = 0;
     }
+
+    window.alert("Bye Bye!");
 }
 
-const displayName = varToString({ someVar })
-console.log(displayName)
+Main();
